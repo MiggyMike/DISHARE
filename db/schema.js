6bc61d0e398484afb89953a6db0e5fe369ce00a3
MICHAEL <MJENNINGS6@gmail.com>
2020-11-08 19:58:32 -0500

    added db connection and schema with setup. created routes folder and files no setup.

---
+++ db/schema.js
@@ -0,0 +1,15 @@
+const { model } = require("mongoose");
+const CommentModel = require("../models/Comments");
+const RecipeModel = require("../models/Recipe");
+const UserModel = require("../models/User");
+// Importing schemas to export them in one file
+
+const Comment = model("comments", CommentModel);
+const Recipe = model("recipies", RecipeModel);
+const User = model("users", UserModel);
+
+module.exports = {
+  Comment,
+  Recipe,
+  User,
+};
