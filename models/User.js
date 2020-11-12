af50c643947f856c5a2949bc1be39ea3382a976f
MICHAEL <MJENNINGS6@gmail.com>
2020-11-12 10:26:36 -0500

    fixed typo in user property/ cause of register route not saving user

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Schema } = require("mongoose");
 
 module.exports = new Schema(
   {
-    mame: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true, index: true },
     password_digest: { type: String, required: true },
   },
