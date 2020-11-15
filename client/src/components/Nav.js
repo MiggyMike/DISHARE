6d7ac597cf20b2b26a2fb93f12141f1533ef132c
MICHAEL <MJENNINGS6@gmail.com>
2020-11-15 02:27:04 -0500

    Nav established and set

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,22 +5,25 @@ export default({ authenticated, currentUser, className }) => {
   return authenticated && currentUser ? (
     <header className={className}>
     <nav className="nav-wrapper">
-      <Link to="#" className="brand-logo center">di/SH/are</Link>
+      <Link to="/" className="brand-logo center">di/SH/are</Link>
       <ul id="nav-mobile" className="left hide-on-med-and-down">
         <li><Link to="/recipes"> Recipes </Link></li>
-        <li><Link to="/users">Profile</Link></li>
-        <li><Link to="/comments">Comments</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/upload">Post Recipe</Link></li>
+        <li><Link activeClassName="nav-active"
+          to="/"
+         >Sign Out</Link></li>
       </ul>
     </nav>
     </header>
   ) : (
     <header className={className}>
     <nav className="nav-wrapper">
-      <Link to="#" className="brand-logo center">di/SH/are</Link>
+      <Link to="/" className="brand-logo center">di/SH/are</Link>
       <ul id="nav-mobile" className="left hide-on-med-and-down">
         <li><Link to="/reicpes">Recipes</Link></li>
-        <li><Link to="/signin">Sign In</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/login">Sign In</Link></li>
+        <li><Link to="/register">Sign Up</Link></li>
       </ul>
     </nav>
     </header>
