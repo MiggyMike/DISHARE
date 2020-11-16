c2a146d11ea6779c599e0a395a3ab8bcda135a9a
MICHAEL <MJENNINGS6@gmail.com>
2020-11-15 21:55:32 -0500

    client commited

---
+++ client/src/services/ApiClient.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const ApiClient = axios.create({ baseURL: "http://localhost:3002/api" });
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+export default ApiClient;
