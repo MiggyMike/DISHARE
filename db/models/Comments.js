62d0087d7eb74a5ea0d1f3270b3eef9f286162a5
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 16:27:13 -0500

    removed rating for now

--- db/models/Comments.js
+++ db/models/Comments.js
@@ -6,12 +6,6 @@ module.exports = new Schema(
       type: String,
       required: true,
     },
-    rating: {
-      type: Number,
-      ratingValue: 5,
-      lowRating: 1,
-      highRating: 5,
-    },
     user_id: {
       type: Schema.Types.ObjectId,
       ref: "users",
