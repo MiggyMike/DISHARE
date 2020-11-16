e95af8cd71986dbfa8e305656f4bbf9c94b78c23
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 12:34:56 -0500

    fixed some stuff for display

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,49 +1,52 @@
-import React, { Component } from 'react'
-import Card from '../components/Card'
-import { __DeleteRecipe } from '../services/RecipeServices'
-import { __GetProfile } from '../services/UserServices'
+import React, { Component } from "react";
+import Card from "../components/Card";
+import { __DeleteRecipe } from "../services/RecipeServices";
+import { __GetProfile } from "../services/UserServices";
 
 export default class Profile extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       recipeFetchError: false,
-      recipes: []
-    }
+      recipes: [],
+    };
   }
 
   componentDidMount() {
-    this.getRecipes()
+    this.getRecipes();
   }
 
   getRecipes = async () => {
     try {
-      console.log(this.props)
-      const profileData = await __GetProfile(this.props.currentUser._id)
-      this.setState({ recipes: profileData.recipes })
+      console.log(this.props);
+      const profileData = await __GetProfile(this.props.currentUser._id);
+      this.setState({ recipes: profileData.recipes });
     } catch (error) {
-      this.setState({ recipeFetchError: true })
+      this.setState({ recipeFetchError: true });
     }
-  }
+  };
 
   deleteRecipe = async (id) => {
     try {
-      const recipesToKeep = this.state.recipes.filter((recipe) => recipe._id !== id)
-      this.setState({ recipes: recipesToKeep })
-      await __DeleteRecipe(id)
+      const recipesToKeep = this.state.recipes.filter(
+        (recipe) => recipe._id !== id
+      );
+      this.setState({ recipes: recipesToKeep });
+      await __DeleteRecipe(id);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   render() {
     return (
       <div className="profile">
         <div>
           {this.state.recipes.length ? (
-            <div className="recipe-content wrapper flex-row">
+            <div className="recipe-content container row ">
+              <div></div>
               {this.state.recipes.map((recipe) => (
-                <div key={recipe._id}>
+                <div key={recipe._id} className="col s3">
                   <Card
                     onClick={() =>
                       this.props.history.push(`/recipe/${recipe._id}`)
@@ -77,6 +80,6 @@ export default class Profile extends Component {
           )}
         </div>
       </div>
-    )
+    );
   }
 }
