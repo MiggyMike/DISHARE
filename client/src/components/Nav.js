065b668f2acee31036b089328e1795f870b7a8e2
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 14:20:36 -0500

    formatting

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default({ authenticated, currentUser, className }) => {
+export default ({ authenticated, currentUser, className }) => {
   return authenticated && currentUser ? (
     <header className={className}>
-    <nav className="nav-wrapper">
-      <Link to="/" className="brand-logo center">di/SH/are</Link>
-      <ul id="nav-mobile" className="left hide-on-med-and-down">
-        <li><Link to="/recipes"> Recipes </Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/upload">Post Recipe</Link></li>
-        <li><Link to="/">Sign Out</Link></li>
-      </ul>
-    </nav>
+      <nav className="nav-wrapper">
+        <Link to="/" className="brand-logo center">
+          di/SH/are
+        </Link>
+        <ul id="nav-mobile" className="left hide-on-small-and-down">
+          <li>
+            <Link to="/recipes"> Recipes </Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/upload">Post Recipe</Link>
+          </li>
+          <li>
+            <Link to="/">Sign Out</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   ) : (
     <header className={className}>
-    <nav className="nav-wrapper">
-      <Link to="/" className="brand-logo center">di/SH/are</Link>
-      <ul id="nav-mobile" className="left hide-on-med-and-down">
-        <li><Link to="/recipes">Recipes</Link></li>
-        <li><Link to="/login">Sign In</Link></li>
-        <li><Link to="/register">Sign Up</Link></li>
-      </ul>
-    </nav>
+      <nav className="nav-wrapper">
+        <Link to="/" className="brand-logo center">
+          di/SH/are
+        </Link>
+        <ul id="nav-mobile" className="left hide-on-small-and-down">
+          <li>
+            <Link to="/recipes">Recipes</Link>
+          </li>
+          <li>
+            <Link to="/login">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/register">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
