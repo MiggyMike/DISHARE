2fcaf83c73b04c5bd343d23e7477b0bf7306400a
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 21:21:18 -0500

    implemented comment section an functionality

--- client/src/pages/ViewRecipe.js
+++ client/src/pages/ViewRecipe.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { __GetRecipe } from "../services/RecipeServices";
+import CreateComment from "../components/CreateComment";
 export default class ViewRecipe extends Component {
   constructor() {
     super();
@@ -15,69 +16,76 @@ export default class ViewRecipe extends Component {
 
   getRecipe = async () => {
     try {
-      const recipe = await __GetRecipe(this.props.match.params.recipe_id);
-      this.setState({ recipe });
+      const foundRecipe = await __GetRecipe(this.props.match.params.recipe_id);
+      this.setState({ recipe: foundRecipe });
     } catch (error) {
       console.log(error);
     }
   };
 
   render() {
+    console.log(this.state.recipe);
     const { recipe } = this.state;
     if (this.state.recipe) {
       return (
         <div className="container recipes row">
-          <div className="content-wrapper card-panel grey lighten-5 z-depth-1 ">
-            <div className="header-content col s12">
-              <h2>{recipe.title}</h2>
-              <div className="img-sum-wrapper row">
-                <div className="image-wrapper col s6">
-                  <img
-                    className="responsive-img"
-                    src={recipe.image_url}
-                    alt="recipe"
-                  />
+          <div>
+            <div className="content-wrapper card-panel grey lighten-5 z-depth-1 ">
+              <div className="header-content col s12">
+                <h2>{recipe.title}</h2>
+                <div className="img-sum-wrapper row">
+                  <div className="image-wrapper col s6">
+                    <img
+                      className="responsive-img"
+                      src={recipe.image_url}
+                      alt="recipe"
+                    />
+                  </div>
+                  <div className="right-content col s6 ">
+                    <p className="recipe-summary">{recipe.summary}</p>
+                    <p>Prep Time: {recipe.prepTime}</p>
+                    <p>Cook Time: {recipe.cookTime}</p>
+                    <p>Total Time: {recipe.totalTime}</p>
+                    <p>Servings: {recipe.servings}</p>
+                  </div>
                 </div>
-                <div className="right-content col s6 ">
-                  <p className="recipe-summary">{recipe.summary}</p>
-                  <p>Prep Time: {recipe.prepTime}</p>
-                  <p>Cook Time: {recipe.cookTime}</p>
-                  <p>Total Time: {recipe.totalTime}</p>
-                  <p>Servings: {recipe.servings}</p>
+              </div>
+              <div className="main-content col-2 flex-col">
+                <div className="content-top">
+                  <h5>Ingredients</h5>
+                  <p>{recipe.ingredients}</p>
+                  <h5>Instructions</h5>
+                  <p>{recipe.instructions}</p>
                 </div>
               </div>
             </div>
-            <div className="main-content col-2 flex-col">
-              <div className="content-top">
-                <h5>Ingredients</h5>
-                <p>{recipe.ingredients}</p>
-                <h5>Instructions</h5>
-                <p>{recipe.instructions}</p>
-                <div className="stats flex-row">
-                  <div>
-                    <p>Likes</p>
-                    <p>{recipe.popularity_rating}</p>
-                  </div>
-                  <div>
-                    <p>Comments</p>
-                    <p>{recipe.comments.length}</p>
-                  </div>
+
+            <div className="card-panel">
+              <CreateComment
+                {...this.props}
+                currentUser={this.props.currentUser}
+                recipeId={this.state.recipe._id}
+              />
+            </div>
+            <div className="card comments-card">
+              <div className="stats row">
+                <div className="col">
+                  <p>Comments: {recipe.comments.length}</p>
                 </div>
               </div>
-              <div className="comments">
-                {recipe.comments.length ? (
-                  recipe.comments.map((comment) => (
-                    <li className="comment-item" key={comment._id}>
-                      <p>
-                        <Link>{comment.user_id.name}</Link>
-                      </p>
-                      <p>{comment.comment}</p>
-                    </li>
-                  ))
-                ) : (
-                  <h3>No Comments</h3>
-                )}
-              </div>
+              <h3>Comments</h3>
+              {recipe.comments.length ? (
+                recipe.comments.map((comment, index) => (
+                  <div className="comment-item" key={index}>
+                    <p>
+                      <Link to="">{comment.user_id.name}</Link>
+                    </p>
+                    <p>{comment.comment}</p>
+                  </div>
+                ))
+              ) : (
+                <h5>No Comments</h5>
+              )}
             </div>
           </div>
         </div>
