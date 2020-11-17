dcf8e85fe8ab4d30ddada4f8dfb78f16fd47bf43
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 21:22:56 -0500

    create comment services

---
+++ client/src/services/CommentServices.js
@@ -0,0 +1,46 @@
+import ApiClient from "./ApiClient";
+
+export const __UploadComment = async (commentText, recipeId, userId) => {
+  try {
+    const res = await ApiClient.post(`/comments/${recipeId}/user/${userId}`, {
+      comment: commentText,
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetComments = async (page, limit) => {
+  try {
+    const res = await ApiClient.get(
+      `/comments?page=${page || 1}&limit=${limit || 10}`
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __UpdateCommment = async (formData, commentId) => {
+  try {
+    const res = await ApiClient.put(
+      `/comments/${commentId}?active=true`,
+      formData
+    );
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __DeleteComment = async (commentId) => {
+  try {
+    const res = await ApiClient.delete(`/comments/${commentId}?active=true`);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
