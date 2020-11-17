98f5827e24b2db407422ad36a0c32a5c4d5e2dc3
MICHAEL <MJENNINGS6@gmail.com>
2020-11-16 22:10:31 -0500

    temporary styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 // import Search from '../components/TextInput'
 // import Card from '../components/Card'
 
-export default ({children}) => {
+export default (props) => {
   return (
-      <div>
-          <h1>Hello</h1>
-      </div>
-      
-  )
-}
+    <div
+      style={{
+        backgroundImage: `url('https://www.forksoverknives.com/wp-content/uploads/plantbased-diet.jpg')`,
+        minHeight: "100vh",
+        width: "100vw",
+        backgroundSize: "cover",
+        paddingTop: "3vh",
+      }}
+    >
+      <h1>Hello</h1>
+    </div>
+  );
+};
