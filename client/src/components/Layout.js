6992827563e33177206f8ecb3211c3afffd8b867
MICHAEL <MJENNINGS6@gmail.com>
2021-02-03 09:42:44 -0500

    made nav sticky

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,18 +1,18 @@
-import React from "react";
-import Nav from "./Nav";
-import Footer from "./Footer";
+import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
 
 export default (props) => (
-  <div>
-    <Nav
-      {...props}
-      authenticated={props.authenticated}
-      currentUser={props.currentUser}
-      className="header-elevated"
-      toggleAuthenticated={props.toggleAuthenticated}
-    />
-    {props.children}
+    <div>
+        <Nav
+            {...props}
+            authenticated={props.authenticated}
+            currentUser={props.currentUser}
+            className='header-elevated'
+            toggleAuthenticated={props.toggleAuthenticated}
+        />
+        {props.children}
 
-    <Footer />
-  </div>
+        <Footer />
+    </div>
 );
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,61 +1,64 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "../styles/Nav.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Nav.css';
 
 export default (props) => {
-  return (
-    <header>
-      {props.authenticated && props.currentUser ? (
-        <nav className="nav-wrapper">
-          <NavLink to="/" className="brand-logo right hide-on-small-and-down">
-            <span className="sitename-font-nav">di/</span>
-            sh
-            <span className="sitename-font-nav">/are</span>
-          </NavLink>
-          <ul id="nav-mobile" className="left">
-            <li>
-              <NavLink to="/recipes"> Recipes </NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile">Profile</NavLink>
-            </li>
-            <li>
-              <NavLink to="/upload">Post Recipe</NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/"
-                onClick={() =>
-                  props.toggleAuthenticated(false, null, () =>
-                    props.history.push("/")
-                  )
-                }
-              >
-                Sign Out
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      ) : (
-        <nav className="nav-wrapper">
-          <NavLink to="/" className="brand-logo right hide-on-small-and-down">
-            <span className="sitename-font-nav">di/</span>
-            <span className="sh-nav">sh</span>
-            <span className="sitename-font-nav">/are</span>
-          </NavLink>
-          <ul id="nav-mobile" className="left ">
-            <li>
-              <NavLink to="/recipes">Recipes</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Sign In</NavLink>
-            </li>
-            <li>
-              <NavLink to="/register">Sign Up</NavLink>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </header>
-  );
+    return (
+        <header>
+            {props.authenticated && props.currentUser ? (
+                <nav className='nav-wrapper sticky'>
+                    <NavLink
+                        to='/'
+                        className='brand-logo right hide-on-small-and-down'>
+                        <span className='sitename-font-nav'>di/</span>
+                        sh
+                        <span className='sitename-font-nav'>/are</span>
+                    </NavLink>
+                    <ul id='nav-mobile' className='left'>
+                        <li>
+                            <NavLink to='/recipes'> Recipes </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/profile'>Profile</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/upload'>Post Recipe</NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to='/'
+                                onClick={() =>
+                                    props.toggleAuthenticated(false, null, () =>
+                                        props.history.push('/')
+                                    )
+                                }>
+                                Sign Out
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            ) : (
+                <nav className='nav-wrapper'>
+                    <NavLink
+                        to='/'
+                        className='brand-logo right hide-on-small-and-down'>
+                        <span className='sitename-font-nav'>di/</span>
+                        <span className='sh-nav'>sh</span>
+                        <span className='sitename-font-nav'>/are</span>
+                    </NavLink>
+                    <ul id='nav-mobile' className='left '>
+                        <li>
+                            <NavLink to='/recipes'>Recipes</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/login'>Sign In</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/register'>Sign Up</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
+    );
 };
