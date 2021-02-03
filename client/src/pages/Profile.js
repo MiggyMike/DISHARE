e8f32f89dd459b48d494b8ee56bc37797ee6b52a
MICHAEL <MJENNINGS6@gmail.com>
2021-02-03 10:31:47 -0500

    adjusted card elements and padding for sticky nav

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,113 +1,121 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { __DeleteRecipe } from "../services/RecipeServices";
-import { __GetProfile } from "../services/UserServices";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { __DeleteRecipe } from '../services/RecipeServices';
+import { __GetProfile } from '../services/UserServices';
 
 export default class Profile extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      recipeFetchError: false,
-      recipes: [],
-      currentUser: props.currentUser.name,
+    constructor(props) {
+        super();
+        this.state = {
+            recipeFetchError: false,
+            recipes: [],
+            currentUser: props.currentUser.name,
+        };
+    }
+
+    componentDidMount() {
+        this.getRecipes();
+    }
+
+    getRecipes = async () => {
+        try {
+            console.log(this.props);
+            const profileData = await __GetProfile(this.props.currentUser._id);
+            this.setState({ recipes: profileData.recipes });
+        } catch (error) {
+            this.setState({ recipeFetchError: true });
+        }
     };
-  }
 
-  componentDidMount() {
-    this.getRecipes();
-  }
+    deleteRecipe = async (id) => {
+        try {
+            const recipesToKeep = this.state.recipes.filter(
+                (recipe) => recipe._id !== id
+            );
+            this.setState({ recipes: recipesToKeep });
+            await __DeleteRecipe(id);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  getRecipes = async () => {
-    try {
-      console.log(this.props);
-      const profileData = await __GetProfile(this.props.currentUser._id);
-      this.setState({ recipes: profileData.recipes });
-    } catch (error) {
-      this.setState({ recipeFetchError: true });
-    }
-  };
+    render() {
+        return (
+            <div className='profile container'>
+                <div className='user-content'></div>
+                <h4 className='icon'>Welcome back {this.state.currentUser}</h4>
 
-  deleteRecipe = async (id) => {
-    try {
-      const recipesToKeep = this.state.recipes.filter(
-        (recipe) => recipe._id !== id
-      );
-      this.setState({ recipes: recipesToKeep });
-      await __DeleteRecipe(id);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  render() {
-    return (
-      <div className="profile container">
-        <div className="user-content"></div>
-        <h4 className="icon">Welcome back {this.state.currentUser}</h4>
-        <div className="row">
-          {this.state.recipes.length ? (
-            <div className="">
-              {this.state.recipes.map((recipe) => (
-                <div key={recipe._id} className="">
-                  <div className="">
-                    <div className=" col s12 m6 l4">
-                      <div className="card hoverable">
-                        <div className="card-image waves-effect waves-block waves-light">
-                          <img
-                            className="activator"
-                            src={recipe.image_url}
-                            alt="current of food"
-                          />
-                        </div>
-                        <div className="card-content">
-                          <span className="card-title activator grey-text text-darken-4">
-                            {recipe.title}
-                            <i className="material-icons right">more_vert</i>
-                          </span>
-                          <p>
-                            <Link
-                              to="#"
-                              onClick={() =>
-                                this.props.history.push(`/recipe/${recipe._id}`)
-                              }
-                            >
-                              View Post
-                            </Link>
-                          </p>
-                        </div>
-                        <div className="card-reveal">
-                          <div className="card-title grey-text text-darken-4">
-                            <i className="material-icons right">close</i>
-                            <span>{recipe.title}</span>
-                          </div>
-                          <div className="row">
-                            <button
-                              className="btn blue col s12"
-                              onClick={() =>
-                                this.props.history.push(`/edit/${recipe._id}`)
-                              }
-                            >
-                              Edit
-                            </button>
-                            <button
-                              className="btn red  col s12"
-                              onClick={() => this.deleteRecipe(recipe._id)}
-                            >
-                              Delete
-                            </button>
-                          </div>
-                        </div>
-                      </div>
+                <div className=''>
+                    <div className='row'>
+                        {this.state.recipes.length ? (
+                            this.state.recipes.map((recipe) => (
+                                <div key={recipe._id} className=''>
+                                    <div className=' col s12 m6 l4'>
+                                        <div className='card small hoverable'>
+                                            <div className='card-image waves-effect waves-block waves-light'>
+                                                <img
+                                                    className='activator'
+                                                    src={recipe.image_url}
+                                                    alt='current of food'
+                                                />
+                                            </div>
+                                            <div className='card-content'>
+                                                <span className='card-title activator grey-text text-darken-4'>
+                                                    {recipe.title}
+                                                    <i className='material-icons right'>
+                                                        more_vert
+                                                    </i>
+                                                </span>
+                                                <p>
+                                                    <Link
+                                                        to='#'
+                                                        onClick={() =>
+                                                            this.props.history.push(
+                                                                `/recipe/${recipe._id}`
+                                                            )
+                                                        }>
+                                                        View Post
+                                                    </Link>
+                                                </p>
+                                            </div>
+                                            <div className='card-reveal'>
+                                                <div className='card-title grey-text text-darken-4'>
+                                                    <i className='material-icons right'>
+                                                        close
+                                                    </i>
+                                                    <span>{recipe.title}</span>
+                                                </div>
+                                                <div className='row'>
+                                                    <button
+                                                        className='btn blue col s12'
+                                                        onClick={() =>
+                                                            this.props.history.push(
+                                                                `/edit/${recipe._id}`
+                                                            )
+                                                        }>
+                                                        Edit
+                                                    </button>
+                                                    <button
+                                                        className='btn red  col s12'
+                                                        onClick={() =>
+                                                            this.deleteRecipe(
+                                                                recipe._id
+                                                            )
+                                                        }>
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className='span message'>No Posts</div>
+                        )}
                     </div>
-                  </div>
                 </div>
-              ))}
             </div>
-          ) : (
-            <div className="span message">No Posts</div>
-          )}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -1,97 +1,133 @@
-import React, { Component } from "react";
-import { __GetRecipes } from "../services/RecipeServices";
-import { Link } from "react-router-dom";
-import "../styles/global.css";
+import React, { Component } from 'react';
+import { __GetRecipes } from '../services/RecipeServices';
+import { Link } from 'react-router-dom';
+import '../styles/global.css';
 
 export default class Recipes extends Component {
-  constructor() {
-    super();
-    this.state = {
-      recipes: [],
-      currentPage: 1,
-    };
-  }
-
-  componentDidMount() {
-    this.getRecipes();
-  }
-
-  getRecipes = async () => {
-    try {
-      const foundRecipes = await __GetRecipes(this.state.currentPage);
-      this.setState({ recipes: foundRecipes });
-    } catch (err) {
-      console.log(err);
+    constructor() {
+        super();
+        this.state = {
+            recipes: [],
+            currentPage: 1,
+        };
     }
-  };
 
-  incrementPage = () =>
-    this.setState(
-      (prevstate) => ({ currentPage: prevstate.currentPage + 1 }),
-      () => this.getRecipes()
-    );
+    componentDidMount() {
+        this.getRecipes();
+    }
 
-  render() {
-    const { recipes } = this.state;
-    return (
-      <div className=" container recipe ">
-        <h2>Popular Recipes</h2>
-        <section className="">
-          {recipes.length ? (
-            recipes.map((recipe) => (
-              <div className="" key={recipe._id}>
-                <div className="card small">
-                  <div className="card-image waves-effect waves-block waves-light">
-                    <img
-                      className="responsive-img"
-                      src={recipe.image_url}
-                      alt=""
-                      onClick={() =>
-                        this.props.history.push(`/recipe/${recipe._id}`)
-                      }
-                    />
-                  </div>
-                  <div className="card-content">
-                    <span className="card-title activator grey-text text-darken-4">
-                      {recipe.title}
-                      <i className="material-icons right">more_vert</i>
-                    </span>
-                    <p>
-                      <Link
-                        className=" blue-text text-lighten-1"
-                        to="#"
-                        onClick={() =>
-                          this.props.history.push(`/recipe/${recipe._id}`)
-                        }
-                      >
-                        View More
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="card-reveal">
-                    <span className="card-title grey-text text-darken-4">
-                      {recipe.title}
-                      <i className="material-icons right">close</i>
-                    </span>
-                    <p>Summary: {recipe.summary.substring(0, 50)}...</p>
-                    <p>Total Time: {recipe.totalTime.substring(0, 50)}...</p>
-                    <p>Servings: {recipe.servings.substring(0, 50)}</p>
-                    <p>Category: {recipe.category.substring(0, 50)}</p>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <h3>No Recipes</h3>
-          )}
-          <button
-            className="btn #42a5f5 blue lighten-1 waves-light waves-effect"
-            onClick={() => this.incrementPage()}
-          >
-            Load More
-          </button>
-        </section>
-      </div>
-    );
-  }
+    getRecipes = async () => {
+        try {
+            const foundRecipes = await __GetRecipes(this.state.currentPage);
+            this.setState({ recipes: foundRecipes });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    incrementPage = () =>
+        this.setState(
+            (prevstate) => ({ currentPage: prevstate.currentPage + 1 }),
+            () => this.getRecipes()
+        );
+
+    render() {
+        const { recipes } = this.state;
+        return (
+            <div className=' container '>
+                <h2>Popular Recipes</h2>
+                <section className=''>
+                    <div className='row'>
+                        {recipes.length ? (
+                            recipes.map((recipe) => (
+                                <div
+                                    className=' col s12 m6 l4'
+                                    key={recipe._id}>
+                                    <div className='card small hoverable'>
+                                        <div className='card-image waves-effect waves-block waves-light'>
+                                            <img
+                                                className='responsive-img'
+                                                src={recipe.image_url}
+                                                alt=''
+                                                onClick={() =>
+                                                    this.props.history.push(
+                                                        `/recipe/${recipe._id}`
+                                                    )
+                                                }
+                                            />
+                                        </div>
+                                        <div className='card-content'>
+                                            <span className='card-title activator grey-text text-darken-4'>
+                                                {recipe.title}
+                                                <i className='material-icons right'>
+                                                    more_vert
+                                                </i>
+                                            </span>
+                                            <p>
+                                                <Link
+                                                    className=' blue-text text-lighten-1'
+                                                    to='#'
+                                                    onClick={() =>
+                                                        this.props.history.push(
+                                                            `/recipe/${recipe._id}`
+                                                        )
+                                                    }>
+                                                    View More
+                                                </Link>
+                                            </p>
+                                        </div>
+                                        <div className='card-reveal'>
+                                            <span className='card-title grey-text text-darken-4'>
+                                                {recipe.title}
+                                                <i className='material-icons right'>
+                                                    close
+                                                </i>
+                                            </span>
+                                            <p>
+                                                Summary:{' '}
+                                                {recipe.summary.substring(
+                                                    0,
+                                                    50
+                                                )}
+                                                ...
+                                            </p>
+                                            <p>
+                                                Total Time:{' '}
+                                                {recipe.totalTime.substring(
+                                                    0,
+                                                    50
+                                                )}
+                                                ...
+                                            </p>
+                                            <p>
+                                                Servings:{' '}
+                                                {recipe.servings.substring(
+                                                    0,
+                                                    50
+                                                )}
+                                            </p>
+                                            <p>
+                                                Category:{' '}
+                                                {recipe.category.substring(
+                                                    0,
+                                                    50
+                                                )}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <h3>No Recipes</h3>
+                        )}
+                    </div>
+                    <button
+                        className='btn #42a5f5 blue lighten-1 waves-light waves-effect'
+                        onClick={() => this.incrementPage()}>
+                        Load More
+                    </button>
+                </section>
+            </div>
+        );
+    }
 }
