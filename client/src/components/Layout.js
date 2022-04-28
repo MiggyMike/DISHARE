41fb1f0940a2d925fac674d4e04dc211273005b9
MICHAEL <MJENNINGS6@gmail.com>
2022-04-28 09:53:12 -0400

    add css for cleaner display space btwn body and footer

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Nav from './Nav';
-import Footer from './Footer';
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 export default (props) => (
-    <div>
-        <Nav
-            {...props}
-            authenticated={props.authenticated}
-            currentUser={props.currentUser}
-            className='header-elevated'
-            toggleAuthenticated={props.toggleAuthenticated}
-        />
-        {props.children}
+  <div>
+    <Nav
+      {...props}
+      authenticated={props.authenticated}
+      currentUser={props.currentUser}
+      className="header-elevated"
+      toggleAuthenticated={props.toggleAuthenticated}
+    />
+    <div className="main-space">{props.children}</div>
 
-        <Footer />
-    </div>
+    <Footer />
+  </div>
 );
